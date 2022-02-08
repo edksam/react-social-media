bd643f6a2787b4d3d797bd1587acbd1dd8e13733
Ebenezer Sam <paakofisam.sam@.gmail.com>
2022-02-08 22:02:51 +0000

    useState

--- src/index.js
+++ src/index.js
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 function App() {
-  function handleInputChange(event) {
-    const inputValue = event.target.value;
-    console.log(inputValue)
-  }
+  const [developer, setDeveloper] = useState({
+    language: 'python',
+    yearsExperience: 0,
+  });
+  // const [language, setLanguage] = useState('python');
+  // const [experience, setExperience] = useState(0);
 
   return (
     <div>
-      <input type="text" onChange={handleInputChange} />
+      <button
+        onClick={() =>
+          setDeveloper({
+            language: 'Javascript',
+            yearsExperience: 0,
+          })
+        }
+      >
+        Change language
+      </button>
+
+      <input
+        type='number'
+        onChange={(event) =>
+          setDeveloper({
+            ...developer,
+            yearsExperience: event.target.value,
+          })
+        }
+      />
+      <p>I have {developer.yearsExperience} years of experience.</p>
+      <p>I am learning {developer.language}</p>
     </div>
-  )
+  );
 }
 
-const rootNode = document.getElementById("root");
+const rootNode = document.getElementById('root');
 ReactDOM.render(<App />, rootNode);
